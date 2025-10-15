0b76b2cce95e967cf001e4b210d5dbf7a3617dda
Mark Brugnoli-Vinten <netniv@hotmail.com>
2025-10-15 02:49:23 +0100

    Another go!

--- macos-launcher/xmake.lua
+++ macos-launcher/xmake.lua
@@ -1,20 +1,20 @@
 add_rules("mode.debug", "mode.release")
 
 target("macOSLauncher")
-do
     add_rules("xcode.application")
     set_kind("binary")
+    set_basename("STFC Community Mod")
 
     add_files("src/**/*.swift")
     add_files("src/*.swift", "src/*.xcassets")
     add_files("deps/PlzmaSDK/swift/*.swift")
     add_files("src/*.cc")
-
+    add_includedirs("deps/PLzmaSDK")
     add_packages("librsync", "PLzmaSDK")
     add_ldflags("-lc++")
-    add_includedirs("deps/PLzmaSDK")
 
     add_values("xcode.bundle_identifier", "com.stfcmod.startrekpatch")
+    add_values("xcode.codesign_identity", "-")
     add_scflags("-Xcc -fmodules", "-Xcc -fmodule-map-file=macos-launcher/src/module.modulemap", "-D SWIFT_PACKAGE",
         { force = true })
     add_values("xcode.bundle_display_name", "Star Trek Fleet Command Community Mod")
