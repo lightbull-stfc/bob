364351eca331e832c4142e4e653a8db5dfbb4886
Mark Brugnoli-Vinten <netniv@hotmail.com>
2025-12-25 15:15:07 +0000

    It's christmas, lets release it

--- mods/src/version.h
+++ mods/src/version.h
@@ -1,10 +1,10 @@
 #pragma once
 
 // clang-format off
-#define VERSION_MAJOR               0
-#define VERSION_MINOR               6
-#define VERSION_REVISION            1
-#define VERSION_PATCH               10
+#define VERSION_MAJOR               1
+#define VERSION_MINOR               0
+#define VERSION_REVISION            0
+#define VERSION_PATCH               0
 
 #define STRINGIFY_(s)               #s
 #define STRINGIFY(s)                STRINGIFY_(s)
