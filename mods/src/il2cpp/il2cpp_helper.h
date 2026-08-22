d7c36c2d637f6b44b6c1952aa23eb565f1eda99b
lightbull <lightbull.stfc@gmail.com>
2026-08-22 20:48:08 +0200

    refactor: add typed IL2CPP runtime invocation helpers
    - support generic argument packing and typed return values
    - handle void, value, and object return types
    - add typed lookup for specially filtered methods
    - migrate loading-screen hooks away from manual argument arrays

--- mods/src/il2cpp/il2cpp_helper.h
+++ mods/src/il2cpp/il2cpp_helper.h
@@ -1,16 +1,22 @@
 #pragma once
 
-#include "il2cpp-functions.h"
-
-#include <il2cpp-api-types.h>
-#include <il2cpp-class-internals.h>
-#include <il2cpp-config.h>
-#include <il2cpp-object-internals.h>
-#include <utils/Il2CppHashMap.h>
+#include <array>
+#include <memory>
+#include <type_traits>
 
 #include <EASTL/span.h>
 #include <EASTL/unordered_map.h>
 #include <EASTL/vector.h>
+
+#include "il2cpp-functions.h"
+#include <il2cpp-config.h>
+#include <il2cpp-api-types.h>
+#include <il2cpp-class-internals.h>
+#include <il2cpp-object-internals.h>
+
+#include <utils/Il2CppHashMap.h>
+
+#include <spdlog/spdlog.h>
 
 #if !_WIN32
 #include <syslog.h>
@@ -26,7 +32,7 @@ public:
     this->propInfo = propInfo;
   }
 
-  bool isValidHelper()
+  bool isValidHelper() const
   {
 #if DEBUG
     return true;
@@ -41,8 +47,8 @@ public:
       return;
     }
 
-    auto set_method         = il2cpp_property_get_set_method((PropertyInfo*)this->propInfo);
-    auto set_method_virtual = il2cpp_object_get_virtual_method((Il2CppObject*)_this, set_method);
+    const auto *set_method         = il2cpp_property_get_set_method(const_cast<PropertyInfo*>(this->propInfo));
+    const auto *set_method_virtual = il2cpp_object_get_virtual_method(static_cast<Il2CppObject*>(_this), set_method);
 
     Il2CppException* exception = nullptr;
     void*            params[1] = {&v};
@@ -56,8 +62,8 @@ public:
       return nullptr;
     }
 
-    auto get_method         = il2cpp_property_get_get_method((PropertyInfo*)this->propInfo);
-    auto get_method_virtual = il2cpp_object_get_virtual_method((Il2CppObject*)_this, get_method);
+    const auto get_method         = il2cpp_property_get_get_method(const_cast<PropertyInfo*>(this->propInfo));
+    const auto get_method_virtual = il2cpp_object_get_virtual_method(static_cast<Il2CppObject*>(_this), get_method);
 
     Il2CppException* exception = nullptr;
     auto             result    = il2cpp_runtime_invoke(get_method_virtual, _this, nullptr, &exception);
@@ -66,19 +72,19 @@ public:
       return nullptr;
     }
 
-    return (T*)(result);
+    return reinterpret_cast<T*>(result);
   }
 
   template <typename T> T* Get(void* _this)
   {
-    auto r = GetRaw<Il2CppObject>(_this);
-    return !r ? nullptr : (T*)(il2cpp_object_unbox(r));
+    const auto r = GetRaw<Il2CppObject>(_this);
+    return !r ? nullptr : static_cast<T*>(il2cpp_object_unbox(r));
   }
 
   template <typename T> T* GetUnboxedSelf(void* _this)
   {
-    auto r = GetRaw<Il2CppObject>(il2cpp_object_unbox((Il2CppObject*)_this));
-    return !r ? nullptr : (T*)(il2cpp_object_unbox(r));
+    const auto r = GetRaw<Il2CppObject>(il2cpp_object_unbox(static_cast<Il2CppObject*>(_this)));
+    return !r ? nullptr : static_cast<T*>(il2cpp_object_unbox(r));
   }
 
 private:
@@ -95,7 +101,7 @@ public:
     this->fieldInfo = fieldInfo;
   }
 
-  bool isValidHelper()
+  bool isValidHelper() const
   {
 #if DEBUG
     return true;
@@ -104,7 +110,7 @@ public:
 #endif
   }
 
-  inline ptrdiff_t offset() const
+  ptrdiff_t offset() const
   {
     return this->fieldInfo->offset;
   }
@@ -123,7 +129,7 @@ public:
     this->fieldInfo = fieldInfo;
   }
 
-  template <typename T> inline T Get() const
+  template <typename T> T Get() const
   {
     T v;
     il2cpp_field_static_get_value(this->fieldInfo, &v);
@@ -145,16 +151,16 @@ public:
 
   template <typename T> T* New()
   {
-    return (T*)il2cpp_object_new(this->cls);
+    return reinterpret_cast<T*>(il2cpp_object_new(this->cls));
   }
 
-  void* GetType()
+  void* GetType() const
   {
-    auto obj = il2cpp_type_get_object(&this->cls->byval_arg);
+    const auto obj = il2cpp_type_get_object(&this->cls->byval_arg);
     return obj;
   }
 
-  bool isValidHelper()
+  bool isValidHelper() const
   {
 #if DEBUG
     return true;
@@ -163,76 +169,106 @@ public:
 #endif
   }
 
-  template <typename T = void> T* GetMethod(const char* name, int arg_count = -1)
+  template <typename T = void> T* GetMethod(const char* name, const int arg_count = -1)
   {
     if (!this->cls) {
       return nullptr;
     }
 
-    auto fn = il2cpp_class_get_method_from_name(this->cls, name, arg_count);
-    if (fn != nullptr) {
-      return (T*)fn->methodPointer;
+    if (const auto fn = il2cpp_class_get_method_from_name(this->cls, name, arg_count); fn != nullptr) {
+      return reinterpret_cast<T*>(fn->methodPointer);
     }
 
     return nullptr;
   }
 
-  template <typename T = void> T* GetVirtualMethod(const char* name, int arg_count = -1)
+  template <typename T = void> T* GetVirtualMethod(const char* name, const int arg_count = -1)
   {
     if (!this->cls) {
       return nullptr;
     }
 
-    auto fn = il2cpp_class_get_method_from_name(this->cls, name, arg_count);
+    const auto fn = il2cpp_class_get_method_from_name(this->cls, name, arg_count);
+    const auto get_method_virtual = il2cpp_object_get_virtual_method(reinterpret_cast<Il2CppObject*>(this), fn);
 
-    auto get_method_virtual = il2cpp_object_get_virtual_method((Il2CppObject*)this, fn);
-
-    return (T*)get_method_virtual->methodPointer;
+    return const_cast<T*>(get_method_virtual->methodPointer);
   }
 
   template <typename R, typename... Args> class InvokerMethod
   {
-
   public:
-    InvokerMethod(const MethodInfo* fn)
+    using InvokeResult = std::conditional_t<std::is_void_v<R>, bool, R>;
+
+    InvokerMethod(const MethodInfo* fn = nullptr)
         : fn(fn)
     {
     }
 
-    R Invoke(void* _this, Args... args)
+    explicit operator bool() const
     {
-      Il2CppException* exception = nullptr;
-      void*            params    = {};
-      auto             result    = il2cpp_runtime_invoke(this->fn, _this, &params, &exception);
+      return fn != nullptr;
+    }
 
-      return *(R*)il2cpp_object_unbox(result);
+    InvokeResult Invoke(void* _this, Args... args) const
+    {
+      if (!fn) return {};
+
+      Il2CppException* exception = nullptr;
+      std::array<void*, sizeof...(Args)> params{ToParameter(args)...};
+      auto* result = il2cpp_runtime_invoke(fn, _this, params.empty() ? nullptr : params.data(), &exception);
+
+      if (exception) {
+        char message[512] = {};
+        il2cpp_format_exception(exception, message, sizeof(message));
+        spdlog::error("Exception occurred while invoking method {}::{}: {}", fn->klass->name, fn->name, message);
+        return {};
+      }
+
+      if constexpr (std::is_void_v<R>) {
+        return true;
+      } else if constexpr (std::is_pointer_v<R>) {
+        return reinterpret_cast<R>(result);
+      } else {
+        if (!result) return {};
+        auto* value = static_cast<R*>(il2cpp_object_unbox(result));
+        return value ? *value : R{};
+      }
     }
 
     const MethodInfo* fn;
+
+  private:
+    template <typename T> static void* ToParameter(T& value)
+    {
+      if constexpr (std::is_pointer_v<std::remove_cvref_t<T>>) {
+        return const_cast<void*>(reinterpret_cast<const void*>(value));
+      } else {
+        return std::addressof(value);
+      }
+    }
   };
 
   template <typename R, typename... Args>
-  const InvokerMethod<R, Args...> GetInvokeMethod(const char* name, int arg_count = -1)
+  InvokerMethod<R, Args...> GetInvokeMethod(const char* name, const int arg_count = sizeof...(Args))
   {
     if (!this->cls) {
-      return nullptr;
+      return {};
     }
 
     auto fn = il2cpp_class_get_method_from_name(this->cls, name, arg_count);
-
     return InvokerMethod<R, Args...>(fn);
   }
 
   const MethodInfo*
   GetMethodInfoSpecial(const char*                                                    name,
-                       std::function<bool(int param_count, const Il2CppType** param)> arg_filter = nullptr)
+                       const std::function<bool(int param_count, const Il2CppType** param)>& arg_filter = nullptr) const
   {
     if (!this->cls) {
       return nullptr;
     }
 
     auto  flags = 0;
-    void* iter  = NULL;
+    void* iter  = nullptr;
     while (const MethodInfo* method = il2cpp_class_get_methods(this->cls, &iter)) {
       if (method->name[0] == name[0] && !strcmp(name, method->name) && ((method->flags & flags) == flags)) {
         if (!arg_filter || arg_filter(method->parameters_count, method->parameters)) {
@@ -243,36 +279,43 @@ public:
     return nullptr;
   }
 
+  template <typename R, typename... Args>
+  InvokerMethod<R, Args...>
+  GetInvokeMethodSpecial(const char*                                                    name,
+                         const std::function<bool(int param_count, const Il2CppType** param)>& arg_filter = nullptr)
+  {
+    return InvokerMethod<R, Args...>(GetMethodInfoSpecial(name, arg_filter));
+  }
+
   template <typename T = void>
   T* GetMethodSpecial(const char*                                                    name,
-                      std::function<bool(int param_count, const Il2CppType** param)> arg_filter = nullptr)
+                      const std::function<bool(int param_count, const Il2CppType** param)>& arg_filter = nullptr)
   {
     if (!this->cls) {
       return nullptr;
     }
-    auto info = GetMethodInfoSpecial(name, arg_filter);
-    if (info) {
-      return (T*)info->methodPointer;
+    if (const auto info = GetMethodInfoSpecial(name, arg_filter)) {
+      return reinterpret_cast<T*>(info->methodPointer);
     }
     return nullptr;
   }
 
-  template <typename T = void> T* GetMethodSpecial2(Il2CppObject* obj, const char* name)
+  template <typename T = void> T* GetMethodFromObject(Il2CppObject* obj, const char* name)
   {
     if (!this->cls) {
       return nullptr;
     }
 
     for (auto i = 0; i < obj->klass->method_count; ++i) {
-      auto method = obj->klass->klass->methods[i];
+      const auto method = obj->klass->klass->methods[i];
       if (method->name[0] == name[0] && !strcmp(name, method->name)) {
-        return (T*)method->methodPointer;
+        return reinterpret_cast<T*>(method->methodPointer);
       }
     }
     return nullptr;
   }
 
-  const MethodInfo* GetMethodInfo(const char* name, int arg_count = -1)
+  const MethodInfo* GetMethodInfo(const char* name, const int arg_count = -1) const
   {
     if (!this->cls) {
       return nullptr;
@@ -281,49 +324,49 @@ public:
     return il2cpp_class_get_method_from_name(this->cls, name, arg_count);
   }
 
-  inline IL2CppPropertyHelper GetProperty(const char* name)
+  IL2CppPropertyHelper GetProperty(const char* name) const
   {
     return IL2CppPropertyHelper{this->cls, il2cpp_class_get_property_from_name(this->cls, name)};
   }
 
-  inline IL2CppFieldHelper GetField(const char* name)
+  IL2CppFieldHelper GetField(const char* name) const
   {
     return IL2CppFieldHelper{this->cls, il2cpp_class_get_field_from_name(this->cls, name)};
   }
 
-  inline IL2CppStaticFieldHelper GetStaticField(const char* name)
+  IL2CppStaticFieldHelper GetStaticField(const char* name) const
   {
     return IL2CppStaticFieldHelper{this->cls, il2cpp_class_get_field_from_name(this->cls, name)};
   }
 
-  inline IL2CppClassHelper GetParent(const char* name)
+  IL2CppClassHelper GetParent(const char* name) const
   {
-    Il2CppClass* pcls = this->cls;
-    if (pcls) {
+    if (Il2CppClass* pCls = this->cls) {
       do {
-        if (pcls->name[0] == name[0] && !strcmp(name, pcls->name)) {
-          return IL2CppClassHelper{pcls};
+        if (pCls->name[0] == name[0] && !strcmp(name, pCls->name)) {
+          return IL2CppClassHelper{pCls};
         }
 
-        pcls = il2cpp_class_get_parent(pcls);
-      } while (pcls);
+        pCls = il2cpp_class_get_parent(pCls);
+      } while (pCls);
     }
+
     return IL2CppClassHelper{nullptr};
   }
 
-  inline IL2CppClassHelper GetNestedType(const char* name)
+  IL2CppClassHelper GetNestedType(const char* name) const
   {
     for (int i = 0; i < this->cls->nested_type_count; ++i) {
-      auto type = this->cls->nestedTypes[i];
+      const auto type = this->cls->nestedTypes[i];
       if (strcmp(type->name, name) == 0) {
-        return type;
+        return IL2CppClassHelper(type);
       }
     }
 
-    return nullptr;
+    return IL2CppClassHelper(nullptr);
   }
 
-  Il2CppClass* get_cls()
+  Il2CppClass* get_cls() const
   {
     return this->cls;
   }
@@ -336,19 +379,19 @@ private:
 
 inline IL2CppClassHelper il2cpp_get_class_helper_impl(const char* assembly, const char* namespacez, const char* name)
 {
-  auto domain    = il2cpp_domain_get();
-  auto assemblyT = il2cpp_domain_assembly_open(domain, assembly);
-  auto image     = il2cpp_assembly_get_image(assemblyT);
+  const auto domain    = il2cpp_domain_get();
+  const auto assemblyT = il2cpp_domain_assembly_open(domain, assembly);
+  const auto image     = il2cpp_assembly_get_image(assemblyT);
 
-  auto cls = il2cpp_class_from_name(image, namespacez, name);
+  const auto cls = il2cpp_class_from_name(image, namespacez, name);
 
   return IL2CppClassHelper{cls};
 }
 
-template <typename T> inline T* il2cpp_get_array_element(Il2CppArray* array, size_t index)
+template <typename T> T* il2cpp_get_array_element(Il2CppArray* array, const size_t index)
 {
-  Il2CppArraySize* n = (Il2CppArraySize*)(array);
-  return (T*)n->vector[index];
+  const auto n = static_cast<Il2CppArraySize*>(array);
+  return static_cast<T*>(n->vector[index]);
 }
 
 extern eastl::unordered_map<Il2CppClass*, eastl::vector<uintptr_t>> tracked_objects;
@@ -375,5 +418,5 @@ public:
 
 template <typename T> T* il2cpp_resolve_icall_typed(const char* name)
 {
-  return (T*)il2cpp_resolve_icall(name);
+  return reinterpret_cast<T*>(il2cpp_resolve_icall(name));
 }
